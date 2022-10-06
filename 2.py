306c47ded4244648fe66c19e7d1a91efd6cbbe5d
Denst22bi3 <tarakanovdenis72@gmail.com>
2022-10-06 22:33:03 +0300

    first commit

---
+++ 2.py
@@ -0,0 +1,6 @@
+kat1=int(input('ведите первый катет'))
+kat2=int(input('ведите второй катет'))
+plosh=(kat1*kat2)/2
+per=(((kat1*kat1)+(kat2*kat2))**0.5)+kat1+kat2
+print('площадь треугольника равна ',plosh)
+print('периметр треугольника равен ',per)
